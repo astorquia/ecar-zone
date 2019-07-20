40f5e764894667b6db15eb1da02baa41b66ae62d
astorquia <astorquia.antonio@gmail.com>
2019-07-20 13:09:45 +0200

    Obtener informacion de listados por id

--- backend/app.js
+++ backend/app.js
@@ -12,21 +12,48 @@ const connection = mysql.createConnection({
 connection.connect();
 
 app.get('/valoraciones', function (req, res) {
-  connection.query('SELECT * FROM `valoracion`', function (error, results, fields) {
+  connection.query('SELECT * FROM `valoracion`', function (error, results) {
     if (error) throw error;
     res.send(results);
   });
 });
 
 app.get('/estaciones', function (req, res) {
-  connection.query('SELECT * FROM `estacion`', function (error, results, fields) {
+  connection.query('SELECT * FROM `estacion`', function (error, results) {
     if (error) throw error;
     res.send(results);
   });
 });
 
 app.get('/imagenes', function (req, res) {
-  connection.query('SELECT * FROM `imagen`', function (error, results, fields) {
+  connection.query('SELECT * FROM `imagen`', function (error, results) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.get('/estacion/:id', function (req, res) {
+  const id = req.params.id;
+  const query = 'SELECT * FROM `estacion` WHERE id = ' + id;
+  connection.query(query, function (error, results) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.get('/valoracion/:id', function (req, res) {
+  const id = req.params.id;
+  const query = 'SELECT * FROM `valoracion` WHERE id = ' + id;
+  connection.query(query, function (error, results) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+app.get('/imagen/:id', function (req, res) {
+  const id = req.params.id;
+  const query = 'SELECT * FROM `imagen` WHERE id = ' + id;
+  connection.query(query, function (error, results) {
     if (error) throw error;
     res.send(results);
   });
