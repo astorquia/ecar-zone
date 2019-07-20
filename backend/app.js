64c03a062289f35d3aa2196ec8c0b6e42710aaa7
astorquia <astorquia.antonio@gmail.com>
2019-07-20 12:31:17 +0200

    test borrar

--- backend/app.js
+++ backend/app.js
@@ -18,20 +18,6 @@ app.get('/valoraciones', function (req, res) {
   });
 });
 
-app.get('/estaciones', function (req, res) {
-  connection.query('SELECT * FROM `estacion`', function (error, results, fields) {
-    if (error) throw error;
-    res.send(results);
-  });
-});
-
-app.get('/imagenes', function (req, res) {
-  connection.query('SELECT * FROM `imagen`', function (error, results, fields) {
-    if (error) throw error;
-    res.send(results);
-  });
-});
-
 app.get('/', function (req, res) {  //   /  <<<  es un endpoint
   res.send('Hello er!');
 });
